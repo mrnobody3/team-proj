b1462c502c472bdb907fb573f9c1634d10fdf8cf
Dmytrii-Shypilov <dmytriishypilov@gmail.com>
2022-03-23 17:07:05 +0200

    initial files upload

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,16 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+      slideMenu: document.querySelector("[slide-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-visible");
+      refs.slideMenu.classList.toggle("slide");
+    }
+  })();
